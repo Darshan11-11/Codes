94674d5841b41b5de6eb935644d661e9c5f31460
Darshan <darshang0511@gmail.com>
2026-08-28 20:26:19 +0530

    reverse a number

---
+++ ReverseNum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ReverseNum{
+    public static void main(String args[]){
+        Scanner db=new Scanner(System.in);
+        int num=db.nextInt();
+        int reverse=0;
+        while(num!=0){
+            int digit=num % 10;
+            reverse=reverse*10 +digit;
+            num=num/10;
+        }
+        System.out.println(reverse);
+    }
+}
